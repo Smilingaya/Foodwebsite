91add3a6ebb7a1e2142765733f7d037de3cfc087
aya <alizeroukiaya0911@gmail.com>
2023-07-23 20:05:50 +0200

    food

---
+++ main.js
@@ -0,0 +1,10 @@
+//swiper
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
